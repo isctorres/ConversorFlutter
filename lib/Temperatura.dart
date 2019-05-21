4e9bb79781f9339435bf7a1f49324705f06a692f
Ruben Torres Frias <ruben.torres@itcelaya.edu.mx>
2019-05-21 13:29:29 -0500

    calculo de C a F

--- lib/Temperatura.dart
+++ lib/Temperatura.dart
@@ -9,9 +9,86 @@ class Temperatura extends StatefulWidget{
 }
 
 class TemperaturaState extends State<Temperatura>{
+
+  TextEditingController CFController = TextEditingController();
+  TextEditingController FCController = TextEditingController();
+  double resultado;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text('TEMPERATURA'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.wb_sunny),
+                  title: Text('Centrigrados a Fahrenheit'),
+                  //subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+                ),
+                TextField(
+                    controller: CFController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder()
+                    )
+                ),
+                ButtonTheme.bar( // make buttons use the appropriate styles for cards
+                  child: ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                        child: const Text('Convertir'),
+                        onPressed: () {
+                          resultado = double.parse(CFController.text) * 1.8 + 32;
+                          return showDialog(
+                              context: context,
+                              builder: (context){ return AlertDialog(content: Text("Resultado : $resultado"));
+                              }
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.wb_sunny),
+                  title: Text('Fahrenheit a Centrigrados'),
+                ),
+                TextField(
+                    controller: FCController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder()
+                    )
+                ),
+                ButtonTheme.bar( // make buttons use the appropriate styles for cards
+                  child: ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                        child: const Text('Convertir'),
+                        onPressed: () { /* ... */ },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
+    );
   }
 }
